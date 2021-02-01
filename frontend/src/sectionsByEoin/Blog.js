65fdd42fc53604d0bf9a28f0d25c0f1fd1f93c8d
eoinlynchcodes <eoin95lynch@gmail.com>
2021-02-01 09:25:44 +0100

    Removed breakages.

--- frontend/src/sectionsByEoin/Blog.js
+++ frontend/src/sectionsByEoin/Blog.js
@@ -4,24 +4,7 @@ function Blog(){
 
     return (
         <div>
-        {/* <section className="">
-            <div className="coloredBox blue">
-              <h2>Blog</h2>
-            </div>
-            <div className="white">
-              <div
-                class="aroundBlog w3-justify"
-                id="retainable-rss-embed"
-                data-rss="https://medium.com/feed/@4handsf"
-                data-maxcols="3"
-                data-layout="grid"
-                data-poststyle="inline"
-                data-readmore="Read the rest..."
-                data-buttonclass="btn btn-primary"
-                data-offset="-100"
-              ></div>
-            </div>
-          </section> */}
+     
         </div>
     );
 }
